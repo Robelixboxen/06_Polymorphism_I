f463fb032b531f6ee5ec75f59ee1a79a2d32e08e
Robert <robelix@gmx.de>
2022-07-26 11:09:12 +0200

    Final

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,37 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        // Worker worker = new Worker();
+        // Queen queen = new Queen();
+        // Drone drone = new Drone();
+
+        /*  wenn die Subklasse mindestens eine Metode von der SuperKlassee überschreibt 
+            dann kann die "Bezeichner" durch den Typ der Superklasse ersetzt werden...   */
+
+        HoneyBee worker = new Worker();
+        HoneyBee queen = new Queen();
+        HoneyBee drone = new Drone();
+
+        output(worker.doYourJob());
+        output(queen.doYourJob());
+        output(drone.doYourJob());
+
+        // Das sollte nicht gehen...
+        HoneyBee bee = new HoneyBee();
+        output(bee.doYourJob());
+
+output("-------------------");
+
+        output(worker.fly());
+        output(queen.fly());
+        output(drone.fly());
      
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
 
 }
 
